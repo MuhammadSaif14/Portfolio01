902a93d2d4426e691a17b648a89a3424036417ed
Muhammad Saif <saifkhaskheli228@gmail.com>
2023-12-14 11:27:25 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const sr = ScrollReveal ({
+    distance: '35px',
+    duration: '2400',
+    reset: true,
+})
+
+sr.reveal('.logo',{delay:410, origin: 'left'});
+sr.reveal('.navlist',{delay:310, origin: 'top'});
+sr.reveal('.top-btnn',{delay:410, origin: 'right'});
+
+sr.reveal('.main-content h4',{delay:310, origin: 'left'});
+sr.reveal('.main-content h1',{delay:410, origin: 'top'});
+sr.reveal('.main-content p',{delay:310, origin: 'right'});
+sr.reveal('.social',{delay:510, origin: 'left'});
+sr.reveal('.main-btnn',{delay:510, origin: 'bottom'});
+
+
+
+
